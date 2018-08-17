ec5ffbad8e3eee45f48aa4ea47657cf068b6cdb1
ygorkagrishin <webygorkagrishin@gmail.com>
2018-08-17 23:33:29 +0300

    Добавлено модальное окно. Информирующие об успешной отправке формы

--- assets/static/scripts/send.js
+++ assets/static/scripts/send.js
@@ -1,12 +1,26 @@
 $(function() {
+    let modal = $('.form__modal'),
+        btn_close = modal.find('.form__btn.form__btn_type_close');
+
+    // Подключаем обработчик события к кнопке модального окна.
+    btn_close.on('click', function () {
+        return modal.hasClass('active') ? 
+        modal.removeClass('active') : false;
+    })
+
     $('.form__form').submit(function(e) {
-        var $form = $(this);
+        let $form = $(this);
         $.ajax({
             type: $form.attr('method'),
             url: $form.attr('action'),
             data: $form.serialize()
         }).done(function() {
-            console.log('success');
+            // Очищаем форму 
+            $form.trigger('reset');
+
+            // Если форма отправелена, то вешаем активный класс на модальное окно
+            if (!modal.hasClass('active'))
+                return modal.addClass('active');
         }).fail(function() {
             alert('Произошла неизвестная ошибка.')
             });
